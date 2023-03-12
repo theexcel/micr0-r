75a347b6d0b7fcadf3116dcb1a56b4991da603e6
theexcel <xclolatunbosun@gmail.com>
2023-03-12 22:03:37 +0100

    changed boiler plate

--- api_routes/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const controller = require('../controllers/appController');
-
-module.exports = (app) => {
-    app.get('/about', controller.about);
-    app.get('/distance/:zipcode1/:zipcode2', controller.getDistance);
-}
