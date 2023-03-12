1304f3cf038b25137ef22b0b4dd7b2f141f49d61
theexcel <xclolatunbosun@gmail.com>
2023-03-12 01:40:51 +0100

    micro commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const routes = require('./api_routes/routes');
+routes(app);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening to port http://localhost${port}`);
+})
