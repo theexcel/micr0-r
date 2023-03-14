e03f1fc13f348a0f896dc95025b45ff157f1addd
theexcel <xclolatunbosun@gmail.com>
2023-03-14 09:06:12 +0100

    testing endpoints

--- dist/app.js
+++ dist/app.js
@@ -19,7 +19,7 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
-app.use('/', indexRouter);
+app.use('/hello', indexRouter);
 app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-import express, {Request, Response, NextFunction} from 'express';
-var router = express.Router();
+import express, { Request, Response, NextFunction } from 'express'
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+	res.json({ "title": "Express" })
+})
 
-module.exports = router;
+module.exports = router
